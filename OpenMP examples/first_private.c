d4b0aabea834007263c1b2f1af8458255bcc665c
Sarah-2021-scu <83244185+Sarah-2021-scu@users.noreply.github.com>
2022-11-08 07:48:46 -0800

    Create first_private.c

---
+++ OpenMP examples/first_private.c
@@ -0,0 +1,23 @@
+#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <stdio.h>
+#define jstart 122 
+
+int main()
+{
+int j = jstart;
+int n = 1000;
+int a[n];
+#pragma omp parallel for firstprivate(j)
+
+  for(int i=1; i<=n; i++) {
+    if(i == 1 || i == n)
+	j = j + 1;
+    a[i] = a[i] + j;
+    printf("%d\n", a[i]);
+}
+
+return 0;
+}
