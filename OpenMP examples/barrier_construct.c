6430c48ace561200ac770316e55842b6fdc40f5b
Sarah-2021-scu <83244185+Sarah-2021-scu@users.noreply.github.com>
2022-11-08 07:44:53 -0800

    Create barrier_construct.c

---
+++ OpenMP examples/barrier_construct.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <omp.h>
+#define SIZE 1000000
+
+int istart = 0;
+int iend = 1000;
+int array1[SIZE];
+int array2[SIZE]; 
+int array3[SIZE];
+
+int array_init(int, int)
+{
+  int i;
+  for (i=istart; i<iend; i++)
+{
+    array1[i] = i;
+    array2[i] = iend - i;
+}
+}
+
+int main()
+{
+  int value, i;
+  #pragma omp parallel private(istart, iend)
+  array_init(istart, iend);
+  for (i=istart; i<=iend; i++)
+  {
+    array3[i] = array2[i] - array1[i];
+  }
+  
+  #pragma omp barrier
+  value = array3[istart] + array3[0];
+  printf("%d", value);
+}   
